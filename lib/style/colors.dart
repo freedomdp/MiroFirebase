2da33a3f89af91f625f7c426643048ebbcf2588e
sergej <sergey.parubets@gmail.com>
2024-02-18 22:49:32 +0200

    update page

---
+++ lib/style/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF095EAE);
+  static const background = Color(0xFFFFFFFF);
+  static const contentBackground = Color(0xFFEBEBEB);
+  static const border = Color(0xFF000000);
+}
---
+++ lib/style/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class H1 {
+  static const h1Style = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+  );
+}
+
+class H2 {
+  static const h2Style = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+}
